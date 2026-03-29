a9612e2b5a838cfd0259a4fd37dfce3c2e119fd1
Дмитрий Мегет <dmitry7181@gmail.com>
2026-03-30 00:17:57 +0300

    Create sw.js file with Service Worker code for offline caching

---
+++ sw.js
@@ -0,0 +1,43 @@
+// sw.js - Service Worker code for offline caching
+
+const CACHE_NAME = 'v1';
+const CACHE_ASSETS = [
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/images/logo.png',
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_ASSETS);
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
